e4d61148ff074f90d652febcf0329b560d6b5d08
Sam Thorogood <sam.thorogood@gmail.com>
2024-08-08 15:39:15 +1000

    bench

--- bench/speed.ts
+++ bench/speed.ts
@@ -9,11 +9,20 @@ const RUNS = 4;
 const CHECK = false;
 
 const raw = fs.readFileSync(process.argv[2] || '1.csv', 'utf-8');
+const hasCRLF = raw.includes('\r\n');
 const canon = parseCSV(raw);
 
-async function timer(name: string, run: () => any) {
+console.info('got raw data', { length: raw.length, hasCRLF });
+
+type TimerOptions = {
+  crlfUnsupported: boolean;
+  trailingBlank: boolean;
+  consumesHeader: boolean;
+}
+
+async function timer(name: string, run: () => any, opts?: Partial<TimerOptions>) {
   console.time(name);
-  let last: unknown;
+  let last: any;
   try {
     for (let i = 0; i < RUNS; ++i) {
       last = await run();
@@ -22,27 +31,45 @@ async function timer(name: string, run: () => any) {
     console.timeEnd(name);
   }
 
-  // TODO: papaparse doesn't include trailing empty fields in rows
-  if (CHECK) {
-    assert.deepStrictEqual(canon, last);
+  if (hasCRLF && opts?.crlfUnsupported) {
+    console.debug('...no CRLF support');
+    return;
   }
+
+  const sampleFirst = last[opts?.consumesHeader ? 0 : 1];
+  let sampleLast: any;
+
+  if (!opts?.trailingBlank) {
+    sampleLast = last.at(-1);
+  } else {
+    let check = last.length - 1;
+    while (check) {
+      if (last[check][0]) {
+        sampleLast = last[check];
+        break;
+      }
+      --check;
+    }
+  }
+
+  console.debug(last.length, 'rows, sample', { first: sampleFirst, last: sampleLast });
 }
 
-await timer('stream', () => {
+await timer('tiddlycsv', () => {
   return parseCSV(raw);
 });
 
 await timer('but-csv', () => {
   return bcs.parse(raw);
-});
+}, { crlfUnsupported: true });
 
-await timer('pp', () => {
+await timer('papaparse', () => {
   const out = pp.default.parse(raw);
   return out.data;
-});
+}, { trailingBlank: true });
 
 await timer('udsv', () => {
   let schema = udsv.inferSchema(raw);
   let parser = udsv.initParser(schema);
   return parser.stringArrs(raw);
-});
+}, { consumesHeader: true });
--- src/run.ts
+++ src/run.ts
@@ -4,9 +4,5 @@ import { parseCSV } from './lowlevel.ts';
 const raw = fs.readFileSync(process.argv[2], 'utf-8');
 const parsed = parseCSV(raw);
 
-const end = raw.substring(raw.length - 512);
-for (let i = 0; i < end.length; ++i) {
-  console.info(end.charCodeAt(i).toString(16), end.charAt(i));
-}
-
 console.info(JSON.stringify(parsed, null, 2));
+console.info('rows', parsed.length);
